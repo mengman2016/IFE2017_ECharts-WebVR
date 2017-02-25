b041e5cd93c924e15c8c473898f86e0d04b39bf1
Mengman <977231991@qq.com>
2017-02-25 10:29:01 +0800

    Update car.js

--- WebGL_No.1_Three.js/car.js
+++ WebGL_No.1_Three.js/car.js
@@ -5,21 +5,22 @@
 var renderer = new THREE.WebGLRenderer({
     canvas:document.getElementById("mainCanvas")
 });
-renderer.setClearColor(0x666666);
+renderer.setClearColor(0x666666);       
 
 var scene = new THREE.Scene();
 
 var camera = new THREE.PerspectiveCamera(50,4/3,1,200);
 camera.position.set(100,70,100);
-camera.lookAt(new THREE.Vector3(0,0,0));
+camera.lookAt(new THREE.Vector3(0,0,0));        
 scene.add(camera);
 
-var material = new THREE.MeshLambertMaterial({color:0xa0a0a0});
+//car
+var material = new THREE.MeshLambertMaterial({color:0xa0a0a0});         //车身车轮材质
 var carBody = new THREE.Mesh(new THREE.CubeGeometry(72,30,36),material);
 scene.add(carBody);
 var carWheel = [];
 for (var i =0; i<4;i++){
-    carWheel[i] = new THREE.Mesh(new THREE.TorusGeometry(8,2.5,60,60),material);
+    carWheel[i] = new THREE.Mesh(new THREE.TorusGeometry(8,2.5,60,60),material);    //4个车轮
     scene.add(carWheel[i]);
 }
 carBody.position.set(0,16,0);
@@ -29,8 +30,8 @@ carWheel[2].position.set(-23,0,-18);
 carWheel[3].position.set(23,0,-18);
 
 //light
-var ambientLight = new THREE.AmbientLight(0xeeeeee);
-var directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+var ambientLight = new THREE.AmbientLight(0xeeeeee);            //环境光
+var directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);   //平行光
 directionalLight.position.set(80,100,40);
 scene.add(ambientLight);
 scene.add(directionalLight);
